576616b3b86ba39b1d15282801e44233dc3f8a35
David Turner <csusbdt@gmailcom>
2019-03-02 17:22:47 -0800

    x

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print("hi")
+import math
+#x = math.sin(math.pi)
+#print(str(round(x,2)))
+#"{0:2g}".format(math.pi)
 
+m = 32767 / 2
+#print(m*math.sin(t))
+
+newFile = open("byte.dat", "wb")
+
+imax = 44100
+f=440
+for i in range(0, imax):
+	t=i/44100
+	s=int(m*math.sin(f*2*math.pi*t))
+	ba=s.to_bytes(2, byteorder='little', signed='true')
+	newFile.write(ba)
+
+print("correct syntax")
---
+++ run.sh
@@ -0,0 +1,1 @@
+ffmpeg -y -hide_banner -f s16le -ar 44100 -ac 1 -i byte.dat wave.wav
